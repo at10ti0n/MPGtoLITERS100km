0589bdafb83b77f1da703986e017492373af337c
Denis Salanta <denis@Deniss-MacBook-Pro.local>
2014-10-09 02:50:31 +0100

    :initial commit
    Signed-off-by: Denis Salanta <denis@Deniss-MacBook-Pro.local>

--- MPGtoLITERS100km/ViewController.swift
+++ MPGtoLITERS100km/ViewController.swift
@@ -10,6 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mpgField : UITextField!
+    @IBOutlet var transformButton : UIButton!
+    @IBOutlet var mpgLabel : UILabel!
+    @IBOutlet var l100kmLabel : UILabel!
+    @IBOutlet var l100kmField : UITextField!
+    @IBOutlet var mpgTol100kmSwitch: UISwitch!
+
+    var km100Lt = 0.425;
+    var mpg = 2.352;
+    //mpg = (km/lt) * 2.352
+    //km/lt = mpg * 0.425
+    var mpgToL100KMDirection: Bool = true;
+    
+    let mpgTransformDescription = "Transform MPG to L100KM";
+    let l100kmTransformDescription = "Transform L100KM to MPG";
+    
+    @IBAction func transformTapped(sender : AnyObject) {
+        if(mpgToL100KMDirection){
+            var mpgValue = Double((mpgField.text as NSString).doubleValue);
+            l100kmField.text = String(format: "%0.2f", mpgValue * km100Lt);
+        }
+        else{
+            var l100kmValue = Double((l100kmField.text as NSString).doubleValue);
+            mpgField.text = String(format: "%0.2f", l100kmValue * mpg);
+        }
+    }
+    
+    @IBAction func mpgTol100kmTapped(sender : AnyObject) {
+        mpgToL100KMDirection = !mpgToL100KMDirection;
+        transformButton.setTitle(mpgToL100KMDirection ? mpgTransformDescription : l100kmTransformDescription, forState: UIControlState.Normal);
+        if(mpgToL100KMDirection){
+            mpgField.becomeFirstResponder();
+        } else {
+            l100kmField.becomeFirstResponder();
+        }
+    }
+    
+    @IBAction func viewTapped(sender : AnyObject) {
+        mpgField.resignFirstResponder();
+        l100kmField.resignFirstResponder();
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
